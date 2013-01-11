6b241c0dceabe868f3c266341a800f3f8b708b50
resteve <resteve@zikzakmedia.com>
2013-01-11 12:17:46 +0100

    Header account_es_pyme in test files

--- tests/test_account_es_pyme.py
+++ tests/test_account_es_pyme.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#This file is part account_es module for Tryton.
+#This file is part account_es_pyme module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 
