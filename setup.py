734ca6b9a3ef5d0629941f3886b601fd923b23e9
resteve <resteve@zikzakmedia.com>
2013-03-05 08:55:00 +0100

    FIX website in setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='trytond_account_es_pyme',
     version=info.get('version', '0.0.1'),
     description='Tryton module with PyME Spanish chart of accounts',
     author='Tryton',
-    url=WEBSITE,
+    url='http://www.tryton.org/',
     download_url="http://downloads.tryton.org/" + \
         VERSION.rsplit('.', 1)[0] + '/',
     package_dir={'trytond.modules.account_es_pyme': '.'},
