a58d1bdff9921a2e940263e7fb59f29f5000d3da
resteve <resteve@zikzakmedia.com>
2013-11-18 09:15:06 +0100

    Change setup.py to use a better structure

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
-
+# encoding: utf-8
+#This file is part account_es_pyme module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 from setuptools import setup
 import re
+import os
 import ConfigParser
+
+MODULE = 'account_es_pyme'
+PREFIX = 'trytond'
+MODULE2PREFIX = {}
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -19,26 +29,31 @@ minor_version = int(minor_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+        prefix = MODULE2PREFIX.get(dep, 'trytond')
+        requires.append('%s_%s >= %s.%s, < %s.%s' %
+                (prefix, dep, major_version, minor_version,
+                major_version, minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytond_account_es_pyme',
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description='Tryton module with PyME Spanish chart of accounts',
+    description=('Tryton module with PyME Spanish chart of accounts'),
+    long_description=read('README'),
     author='Tryton',
     url='http://www.tryton.org/',
-    download_url=("http://downloads.tryton.org/" +
-        info.get('version', '0.0.1').rsplit('.', 1)[0] + '/'),
-    package_dir={'trytond.modules.account_es_pyme': '.'},
+    download_url='https://bitbucket.org/trytonspain/trytond-%s' % MODULE,
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.account_es_pyme',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.account_es_pyme': info.get('xml', []) \
-            + ['tryton.cfg'],
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -48,19 +63,28 @@ setup(name='trytond_account_es_pyme',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
+        'Natural Language :: Czech',
+        'Natural Language :: Dutch',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Natural Language :: German',
+        'Natural Language :: Russian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Office/Business',
-        'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
     install_requires=requires,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_es_pyme = trytond.modules.account_es_pyme
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
+    test_suite='tests',
+    test_loader='trytond.test_loader:Loader',
+    tests_require=tests_require,
     )
