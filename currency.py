7a4ceb8c9dbe09fbc7654e1ee8504d0400381ac5
Raimon Esteve <raimon@nan-tic.com>
2017-03-30 12:28:48 +0200

    Use the same class files as account_es module

--- currency.py
+++ currency.py
@@ -1,7 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from decimal import ROUND_HALF_UP
-
 from trytond.pool import PoolMeta
 
 __all__ = ['Currency']
---
+++ tax.py
@@ -0,0 +1,33 @@
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['TaxTemplate', 'Tax']
+
+
+class TaxTemplate:
+    __metaclass__ = PoolMeta
+    __name__ = 'account.tax.template'
+
+    report_description = fields.Text('Report Description', translate=True)
+    recargo_equivalencia = fields.Boolean('Recargo Equivalencia',
+        help='Indicates if the tax is Recargo de Equivalencia')
+
+    def _get_tax_value(self, tax=None):
+        res = super(TaxTemplate, self)._get_tax_value(tax)
+
+        if not tax or tax.report_description != self.report_description:
+            res['report_description'] = self.report_description
+        if not tax or tax.recargo_equivalencia != self.recargo_equivalencia:
+            res['recargo_equivalencia'] = self.recargo_equivalencia
+        return res
+
+
+class Tax:
+    __metaclass__ = PoolMeta
+    __name__ = 'account.tax'
+
+    report_description = fields.Text('Report Description', translate=True)
+    recargo_equivalencia = fields.Boolean('Recargo Equivalencia',
+        help='Indicates if the tax is Recargo de Equivalencia')
