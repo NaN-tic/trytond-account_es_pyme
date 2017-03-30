ebee45e82e3f822c4a0fb66dfd60bab16f1cf4f9
Raimon Esteve <raimon@nan-tic.com>
2017-03-30 12:28:48 +0200

    Use the same class files as account_es module

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-
 from trytond.pool import Pool
-from .account import *
-from .currency import *
+from . import account
+from . import currency
+from . import tax
 
 
 def register():
     Pool.register(
-        Currency,
-        Account,
-        TaxTemplate,
-        Tax,
+        account.Account,
+        currency.Currency,
+        tax.Tax,
+        tax.TaxTemplate,
         module='account_es_pyme', type_='model')
