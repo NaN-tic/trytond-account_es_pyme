81f37c78cd67e91ac2011a0e98d8eb054ba4d6c4
C?dric Krier <ced@b2ck.com>
2018-01-08 19:13:55 +0100

    Fix style

--- account.py
+++ account.py
@@ -1,5 +1,5 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
 
@@ -14,7 +14,7 @@ class Account:
     def __setup__(cls):
         super(Account, cls).__setup__()
         value = ('efective', 'Efective')
-        if not value in cls.kind.selection:
+        if value not in cls.kind.selection:
             cls.kind.selection.append(value)
 
 
